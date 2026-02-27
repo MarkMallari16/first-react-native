c99d7a15423e214cca9c8c4ee9cd6cac96d4835f
Mark Christian Mallari <mallariftw01@gmail.com>
2026-02-27 12:11:53 +0800

    feat: Add practice counter app

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,93 +1,75 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { ExternalLink } from '@/components/external-link';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Collapsible } from '@/components/ui/collapsible';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Fonts } from '@/constants/theme';
+import { AntDesign } from '@expo/vector-icons';
+import { Image } from 'expo-image';
+import { useState } from 'react';
+import { Button, Linking, StyleSheet, TouchableOpacity, View } from 'react-native';
 
 export default function TabTwoScreen() {
+  const [count, setCount] = useState<number>(0);
+
+  const goToGithub = async () => {
+    const url = "https://github.com/MarkMallari16";
+    const supported = await Linking.canOpenURL(url);
+
+    if (supported) {
+      await Linking.openURL(url);
+    } else {
+      alert(`Don't know how to open this URL: ${url}`);
+    }
+  }
+
+  const decrement = () => {
+    if (count != 0) {
+      setCount(count - 1);
+    }
+  }
+
+  const reset = () => {
+    setCount(0);
+  }
+
+  const increment = () => {
+    setCount(count + 1);
+  }
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#1f1f1f' }}
+      headerBackgroundColor={{ light: '#f1f1f1', dark: '#1f1f1f' }}
       headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
         <Image
-          source={require('@/assets/images/react-logo.png')}
-          style={{ width: 100, height: 100, alignSelf: 'center' }}
-        />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user&apos;s current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful{' '}
-          <ThemedText type="defaultSemiBold" style={{ fontFamily: Fonts.mono }}>
-            react-native-reanimated
-          </ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
+          source={require('@/assets/images/dubai-chewy.jpg')}
+          style={styles.headerImage}
+        />}>
+
+      <View style={styles.iconContainer}>
+        <AntDesign name="plus" size={40} color="#f1f1f1" />
+        <AntDesign name="minus" size={40} color="#f1f1f1" />
+      </View>
+
+      <ThemedText type='title'>Basic Counter App</ThemedText>
+
+      <View style={styles.countContainer}>
+        <ThemedText type='title'>{count}</ThemedText>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title='Decrement'
+          color='#ff6467'
+          onPress={decrement} />
+        <Button title='Reset'
+          color='#cccccc'
+          onPress={reset} />
+        <Button title='Increment'
+          color='#50a2ff'
+          onPress={increment} />
+      </View>
+
+      <View style={styles.githubContainer}>
+        <ThemedText type='default'>Follow me on GitHub</ThemedText>
+        <TouchableOpacity onPress={goToGithub} >
+          <AntDesign name='github' size={20} color='#f1f1f1' />
+        </TouchableOpacity>
+      </View>
+
     </ParallaxScrollView>
   );
 }
@@ -99,8 +81,28 @@ const styles = StyleSheet.create({
     left: -35,
     position: 'absolute',
   },
-  titleContainer: {
+  iconContainer: {
     flexDirection: 'row',
-    gap: 8,
+    justifyContent: 'center',
+    gap: 10,
+    marginTop: 20,
+    alignItems: 'center'
   },
+  countContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 10,
+    marginTop: 5,
+  },
+  githubContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 6,
+    marginTop: 20
+  }
 });
